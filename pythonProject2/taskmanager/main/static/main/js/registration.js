b9b619f595a321d6482707f1e3a5837a18720c3f
DanRo23 <120567600+DanRo23@users.noreply.github.com>
2022-12-20 12:12:59 +0300

    Add files via upload

--- pythonProject2/taskmanager/main/static/main/js/registration.js
+++ pythonProject2/taskmanager/main/static/main/js/registration.js
@@ -6,57 +6,23 @@ function authorisation(login, password)
 
 	 xhr.open("POST", 'http://127.0.0.1:8000/signin?', true);
 	 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	 xhr.send(params);
+	 xhr.send(params); 
 	 	xhr.onload = function(){
 		if (xhr.status != 200){
 			alert('Ошибка ${xhr.status} : ${xhr.statusText}');
 		}
 		else {
+			getJson = xhr.responseText;
+			const obj = JSON.parse(getJson);
+			console.log(obj["status"]);
+			if (obj["status"] == true){
+				document.location = "menu";
+			}
 			alert(xhr.responseText);
 		}
 	}
-	 alert("POST succeed DAN");
-//	var xhr = new XMLHttpRequest();
-//	var params = 'login=' + encodeURIComponent(login) +'&password=' + encodeURIComponent(password);
-//	xhr.open('GET', 'http://127.0.0.1:8000/signin?' + params);
-//	xhr.setRequestHeader('Content-type', 'text/html');
-//	// xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
-//	xhr.setRequestHeader('Access-Control-Allow-Origin', 'GET, POST');
-//	xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-//	xhr.setRequestHeader('Access-Control-Allow-Origin', 'Origin, Content-Type, X-Auth-Token');
-//	xhr.send();
-//	alert("GET");
-//	xhr.onload = function(){
-//		if (xhr.status != 200){
-//			alert('Ошибка ${xhr.status} : ${xhr.statusText}');
-//		}
-//		else {
-//			alert('Готов, получили ${xhr.response.length} байт');
-//		}
-//	}
-//	alert("SM");
+	return false;
 }
-// var params = 'login=' + encodeURIComponent(login) +'&password=' + encodeURIComponent(password);
-
-//   var getJSON = function(url, callback) {
-//     var xhr = new XMLHttpRequest();
-//     var url = 'http://127.0.0.1:8000/signin?';
-//     xhr.open('GET', url, true);
-//     xhr.send(params);
-//     xhr.responseType = 'json';
-//     xhr.onload = function() {
-//       var status = xhr.status;
-//       if (status === 200) {
-//         callback(null, xhr.response);
-//         } 
-//         else {
-//           callback(status, xhr.response);
-//       }
-//     };
-//     xhr.send();
-//   };
-
-//   alert(getJSON);
 function enter()
 {
 	let login = document.querySelectorAll("#login_input > input");
@@ -68,12 +34,41 @@ function enter()
 	if (login[0].value == "" || password[0].value == "")
 		document.location="main";
 	else
-		authorisation(login[0].value, password[0].value);
+	{
+		console.log("else");
+		if (authorisation(login[0].value, password[0].value) == true)
+		{
+			console.log("auth");
+			document.location = "menu";
+		}
+	}
 }
 
 function send_registration_data(email, password)
 {
-	console.log(email, password);
+	alert("send" + email + " " + password);
+	var xhr = new XMLHttpRequest();
+	var params = 'email=' + encodeURIComponent(email) +'&password=' + encodeURIComponent(password);
+
+
+	xhr.open("POST", 'http://127.0.0.1:8000/registrationRe?', true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(params);
+
+ 	xhr.onload = function(){
+		if (xhr.status != 200){
+			alert('Ошибка ${xhr.status} : ${xhr.statusText}');
+		}
+		else {
+			getJson = xhr.responseText;
+			const obj = JSON.parse(getJson);
+			console.log(obj["status"]);
+			if (obj["status"] == true){
+				alert("Письмо с подтверждением отправлено на вашу почту");
+				document.location = "main";
+			}
+		}
+	}
 }
 
 function send_profile_data()
@@ -86,14 +81,27 @@ function send_profile_data()
 	let school = document.querySelectorAll("#school > input");
 	let form = document.querySelectorAll("#form > input");
 
-//	console.log(lastname[0].value, firstname[0].value, secondname[0].value, nickname[0].value, datebirth[0].value, school[0].value, form[0].value);
-	console.log(lastname[0].value);
-	console.log(firstname[0].value);
-	console.log(secondname[0].value);
-	console.log(nickname[0].value);
-	console.log(datebirth[0].value);
-	console.log(school[0].value);
-	console.log(form[0].value);
+	var xhr = new XMLHttpRequest();
+	var params = 'nickname=' + encodeURIComponent(nickname) +'&surname=' + encodeURIComponent(lastname) + "&name=" + encodeURIComponent(firstname) + "&secondname=" + encodeURIComponent(secondname) + "&school=" + encodeURIComponent(school) + "&form=" + encodeURIComponent(form) + "&datebirth=" + encodeURIComponent(datebirth);
+
+
+	xhr.open("POST", 'http://127.0.0.1:8000/profileData?', true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send(params);
+
+	xhr.onload = function(){
+		if (xhr.status != 200){
+			alert('Ошибка ${xhr.status} : ${xhr.statusText}');
+		}
+		else {
+			getJson = xhr.responseText;
+			const obj = JSON.parse(getJson);
+			console.log(obj["status"]);
+			if (obj["status"] == true){
+				document.location = "main";
+			}
+		}
+	}
 }
 
 function register()
@@ -130,8 +138,6 @@ function register()
 		text.setAttribute("class", "note")
 
 		send_registration_data(email[0].value, password[0].value);
-
-		document.body.appendChild(text);
 	}
 }
 
